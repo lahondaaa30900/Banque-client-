39f9dc90caecbe9bf954cf5bf38a6cabd5587d62
eliesse.belbey <iliesse82.ib@gmail.com>
2024-02-20 17:11:20 +0100

    modif

---
+++ bdd.js
@@ -0,0 +1,13 @@
+// database.js
+
+// Simulation d'une base de données avec des identifiants valides
+var database = {
+    "user1@example.com": "user1",
+    "user2@example.com": "user2",
+    "user3@example.com": "user3",
+    "user4@example.com": "user4",
+    "user5@example.com": "user5"
+};
+
+// Exportez la base de données pour qu'elle soit accessible depuis d'autres fichiers
+module.exports = database;
