550e22b2616a2073c529e2271c55c75d894210b5
eliesse.belbey <iliesse82.ib@gmail.com>
2024-02-21 15:12:50 +0100

    conenxion

--- Scriptdeconnexion.js
+++ Scriptdeconnexion.js
@@ -1,32 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var loginForm = document.getElementById('loginForm');
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+    
+    // Récupérer les valeurs du formulaire
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
 
-    loginForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        // Simulation de la base de données
-        var database = [
-            { email: 'exemple1@example.com', password: 'motdepasse1' },
-            { email: 'exemple2@example.com', password: 'motdepasse2' },
-            { email: 'exemple3@example.com', password: 'motdepasse3' },
-            { email: 'exemple4@example.com', password: 'motdepasse4' }
-            // Ajoutez d'autres utilisateurs au besoin
-        ];
-
-        var emailInput = document.getElementById('email').value;
-        var passwordInput = document.getElementById('password').value;
-
-        // Vérifiez les identifiants par rapport à la base de données
-        var utilisateurTrouve = database.find(function (utilisateur) {
-            return utilisateur.email === emailInput && utilisateur.password === passwordInput;
-        });
-
-        if (utilisateurTrouve) {
-            // Redirige l'utilisateur vers la page d'accueil après la connexion réussie
-            window.location.href = 'accueil.html';
-        } else {
-            // Gérez le cas où les identifiants ne sont pas valides (par exemple, affichez un message d'erreur)
-            console.log('Votre identifiant ou mot de passe est incorrect.');
-        }
-    });
+    // Vérifier si les identifiants sont valides
+    if (database.hasOwnProperty(email) && database[email] === password) {
+        // Rediriger vers la page d'accueil si les identifiants sont valides
+        window.location.href = "accueil.html";
+    } else {
+        // Afficher un message d'erreur si les identifiants sont incorrects
+        alert("Adresse e-mail ou mot de passe incorrect.");
+    }
 });
